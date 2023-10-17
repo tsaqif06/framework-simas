ff73aa18cf7f0ed775a029c2b6c9365bd07f8b97
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2023-10-17 14:28:14 +0700

    Add partials views and Add upload image function

--- app/Config/Model.php
+++ app/Config/Model.php
@@ -50,6 +50,64 @@ class Model
         return $stmt->rowCount();
     }
 
+
+    public function uploadImage()
+    {
+        if (empty($_FILES)) {
+            return;
+        }
+
+        $targetDir = "public/assets/img/uploads/";
+        $targetFile = $targetDir . basename($_FILES["fileToUpload"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+        // Periksa apakah file gambar nyata atau bukan
+        if (isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+            if ($check !== false) {
+                echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                echo "File is not an image.";
+                $uploadOk = 0;
+            }
+        }
+
+        // Periksa apakah file sudah ada
+        if (file_exists($targetFile)) {
+            echo "Sorry, file already exists.";
+            $uploadOk = 0;
+        }
+
+        // Batasi ukuran file
+        if ($_FILES["fileToUpload"]["size"] > 500000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+
+        // Izinkan hanya beberapa format file tertentu
+        if (
+            $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif"
+        ) {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+
+        // Cek apakah $uploadOk bernilai 0 oleh suatu kesalahan
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+            // Jika semuanya beres, coba upload file
+        } else {
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
+                echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
+    }
+
     public function update($id, $data = [])
     {
         $data['updated_at'] = date('Y-m-d H:i:s');
---
+++ resources/views/partials/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User</title>
+    <style>
+        table,
+        tr,
+        td {
+            border: 1px solid black;
+        }
+    </style>
+</head>
+
+<body>
