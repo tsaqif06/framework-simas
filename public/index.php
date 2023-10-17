2615dd63a8c623874903e6ebc63b59257393194b
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2023-10-17 09:45:52 +0700

    Update APP_DEBUG settings

--- public/index.php
+++ public/index.php
@@ -3,13 +3,14 @@
 use Dotenv\Dotenv;
 use FrameworkSimas\Config\Flasher;
 
-require_once __DIR__ . "/../app/Config/Bootstrap.php";
 require_once __DIR__ . "/../vendor/autoload.php";
-require_once __DIR__ . "/../app/Config/Functions.php";
 $dotenv = Dotenv::createImmutable(__DIR__ . "/../");
 $dotenv->load();
+
+require_once __DIR__ . "/../app/Config/Bootstrap.php";
+require_once __DIR__ . "/../app/Config/Functions.php";
 define("BASEURL", $_ENV['BASE_URL']);
 $flasher = Flasher::flash();
 $GLOBALS['flasher'] = $flasher;
 
-require_once __DIR__ . '/../routes/route.php';
+require_once __DIR__ . '/../rutes/route.php';
