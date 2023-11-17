8f2f9065f935b1eeebe01a21079f844aae76fd49
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2023-11-17 10:16:24 +0700

    Change serve message

--- app/Command/ServeCommand.php
+++ app/Command/ServeCommand.php
@@ -13,7 +13,7 @@ class ServeCommand
         $yellow = "\033[1;33m";
         $url = "http://localhost:$port";
 
-        output("Server is running on {$yellow}{$url}", "blue");
+        output("Server is running on {$yellow}{$url}\n", "blue");
 
         if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
             exec("start $url");
